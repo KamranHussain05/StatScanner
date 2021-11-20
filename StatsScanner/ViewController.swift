ade5329c1dfe22d90a659617c5dbf2ae20ae52fe
Kam <kamran.hssn05@gmail.com>
2021-11-20 11:09:55 -0800

    Initial Commit

---
+++ StatsScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StatsScanner
+//
+//  Created by Kamran on 11/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

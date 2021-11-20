43f9bb43a3ee14ffe89067eb4207fcbaa403ba54
kaleb <kalebkim1939@gmail.com>
2021-11-20 12:55:19 -0800

    i am in pain

---
+++ StatsScanner/CalculatorWrapper.mm
@@ -0,0 +1,14 @@
+//
+//  CalculatorWrapper.m
+//  StatsScanner
+//
+//  Created by Kamran on 11/20/21.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "CalculatorWrapper.h"
+#import "Calculator.hpp"
+
+@implementation CalculatorWrapper : NSObject
+@end

5fbf6e0ba2f60cdd92232ff289e40b79441cc68b
kaleb <kalebkim1939@gmail.com>
2021-12-17 22:43:54 -0800

    testing some gui stuff

--- StatsScanner/ViewController.swift
+++ StatsScanner/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.cyan;
     }
 
     @IBOutlet weak var txt: UILabel!

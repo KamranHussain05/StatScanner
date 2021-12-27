e44613e5fe84776ca268b7170c004750c0cfa447
Kam <kamran.hssn05@gmail.com>
2021-12-27 12:07:22 -0800

    Organized Project and Added Dataset Testing and CSV Reading

--- StatsScanner/DataBridge.swift
+++ StatsScanner/DataBridge.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
+extension String {
+    var isNumber: Bool {
+        return !isEmpty && rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil
+    }
+}
+
 class DataBridge {
+    
     func writeData(data:String) {
         let str = data
         let url = getDocumentsDirectory().appendingPathComponent("message.txt")
@@ -26,4 +33,41 @@ class DataBridge {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
     }
+    
+
+    func readCSV(inputFile: URL, lineSeparator: String, valSeparator: String) -> [[String]] {
+        //Get Data
+        print("reading csv")
+        var result: [[String]] = []
+        do {
+            let data = try String(contentsOf: inputFile)
+            let rows = data.components(separatedBy: CharacterSet(charactersIn: lineSeparator))
+            for row in rows {
+                let columns = row.components(separatedBy: valSeparator)
+                result.append(columns)
+            }
+            print("copying cleaned data")
+            result = clean(data: result)
+            return result
+        } catch {
+            // Any Errors will go here
+            return [["ERROR: File Could Not be Found"]]
+        }
+    }
+    
+    private func clean(data: [[String]]) -> [[String]] {
+        var result = data
+        print("cleaning data")
+        for i in 0...data.count-1 {
+            for j in 0...data[i].count-1 {
+                if (data[i][j].contains("\n") || data[i][j].contains("\r")) {
+                    result[i][j] = data[i][j].replacingOccurrences(of: "\r", with: "")
+                    result[i][j] = data[i][j].replacingOccurrences(of: "\n", with: "")
+                }
+            }
+        }
+        
+        return result
+    }
+
 }
--- StatsScannerTests/StatsScannerTests.swift
+++ StatsScannerTests/StatsScannerTests.swift
@@ -13,7 +13,8 @@ class StatsScannerTests: XCTestCase {
     
     let d = DataSet()
     let b = DataBridge()
-
+    
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         print("==== STARTING TEST ====")
@@ -41,22 +42,41 @@ class StatsScannerTests: XCTestCase {
         b.writeData(data: "test string")
     }
     
-    func testReading() throws {
-        let str = "id,num_awards,prog,math\n45,1,3,41\n108,1,1,41\n15,1,3,44\n67,1,3,42"
-        let url = b.getDocumentsDirectory().appendingPathComponent("message.csv")
+    func testWriting() throws {
+        let str =
+        """
+        id,num_awards,prog,math
+        45,1,3,41
+        108,1,1,41
+        15,1,3,44
+        67,1,3,42
+        """
+        let url = b.getDocumentsDirectory().appendingPathComponent("test.csv")
         
         do {
             try str.write(to: url, atomically: true, encoding: .utf8)
-            
-            let input = try String(contentsOf: url)
-            print(input)
         } catch {
             print(error.localizedDescription)
         }
     }
     
+    func testReading() throws {
+        let url = b.getDocumentsDirectory().appendingPathComponent("test.csv")
+        print(b.readCSV(inputFile: url, lineSeparator: "\n", valSeparator: ","))
+    }
+    
     func testDataSet() throws {
-        //test data set here with multiple cases
+        print("=== Test Case 1 ===")
+        var url = URL(fileURLWithPath: "/Users/kamran/Desktop/Programming/StatsScanner/StatsScanner/Resources/Awards_R.csv")
+        print(b.readCSV(inputFile: url, lineSeparator: "\r", valSeparator: ","))
+        
+        print("=== Test Case 2 ===")
+        url = URL(fileURLWithPath: "/Users/kamran/Desktop/Programming/StatsScanner/StatsScanner/Resources/Video_R.csv")
+        print(b.readCSV(inputFile: url, lineSeparator: "\r", valSeparator: ","))
+        
+        print("=== Test Case 3 ===")
+        url = URL(fileURLWithPath: "/Users/kamran/Desktop/Programming/StatsScanner/StatsScanner/Resources/Diet_R.csv")
+        print(b.readCSV(inputFile: url, lineSeparator: "\r", valSeparator: ","))
     }
 
 }

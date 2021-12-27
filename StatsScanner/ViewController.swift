c0898edc8a5dc2105c840e43ed7b3e83fb7df332
kaleb <kalebkim1939@gmail.com>
2021-12-26 17:35:17 -0800

    using uikit instead of avkit

--- StatsScanner/ViewController.swift
+++ StatsScanner/ViewController.swift
@@ -2,118 +2,46 @@
 //  ViewController.swift
 //  StatsScanner
 //
-//  Created by Kalb on 9/11/21.
+//  Created by Kalb on 12/21/21.
 //
 
 import UIKit
-import AVKit
 
-class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
-    let alertBox = UIAlertController(title: "Error", message: "Default message", preferredStyle: .alert)
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var cameraButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.cyan
         
-    }
-
-    @IBOutlet weak var txt: UILabel!
-    @IBAction func field(_ sender: UITextField) {
-    }
-    
-    @IBAction func TakePictureButton(_ sender: UIButton) {
-        openCamera()
-    }
-    
-    @IBAction func swit(_ sender: UISwitch) {
-        if(sender.isOn) {
-            txt.text = "Among Us."
-        } else {
-            txt.text = "no"
-        }
-    }
-    
-    private func openCamera() {
-        // check authorization status
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized:
-            self.setupCameraSession()
-        case .notDetermined:
-            AVCaptureDevice.requestAccess(for: .video) { (granted) in
-                if granted {
-                    DispatchQueue.main.async {
-                        self.setupCameraSession()
-                    }
-                } else {
-                    self.handleDismiss()
-                }
-            }
-            
-        case .restricted:
-            alertBox.message = "You have something restricting you from opening the camera."
-            self.present(alertBox, animated: true, completion: nil)
-            self.handleDismiss()
-        case .denied:
-            alertBox.message = "You have denied access to the camera previously."
-            self.present(alertBox, animated: true, completion: nil)
-            self.handleDismiss()
-        @unknown default:
-            alertBox.message = "There is some unknown reason why you can't open the camera, hmn"
-            self.present(alertBox, animated: true, completion: nil)
-            self.handleDismiss()
-        }
-    }
-    
-    private let photoOutput = AVCapturePhotoOutput()
-    private func setupCameraSession() {
-        let captureSession = AVCaptureSession()
+        imageView.backgroundColor = .secondarySystemBackground
         
-        if let captureDevice = AVCaptureDevice.default(for: .video) {
-            do {
-                let input = try AVCaptureDeviceInput(device: captureDevice)
-                if captureSession.canAddInput(input) {
-                    captureSession.addInput(input)
-                }
-            } catch let error {
-                print("There was some error: \(error)")
-            }
-            
-            if captureSession.canAddOutput(photoOutput) {
-                captureSession.addOutput(photoOutput)
-            }
-            
-            let cameraLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            cameraLayer.frame = self.view.frame
-            cameraLayer.videoGravity = .resizeAspectFill
-            self.view.layer.addSublayer(cameraLayer)
-            
-            captureSession.startRunning()
-            self.setupUI()
-        }
+        cameraButton.backgroundColor = .systemBlue
+        cameraButton.setTitle("Take Picture", for: .normal)
+        cameraButton.setTitleColor(.white, for: .normal)
     }
     
-    @objc private func handleTakePhoto() {
-        let photoSettings = AVCapturePhotoSettings()
-        if let photoPreviewType = photoSettings.availablePreviewPhotoPixelFormatTypes.first {
-            photoSettings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: photoPreviewType]
-            photoOutput.capturePhoto(with: photoSettings, delegate: self)
-        }
-    }
-    
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let imageData = photo.fileDataRepresentation() else {return}
-        let previewImage = UIImage(data: imageData)
-        
-//        let photoPreviewContainer = UIImage
-        photoPreviewContainer.photoImageView
-    }
-    
-    private func setupUI() {
-        
-    }
-    
-    private func handleDismiss() {
-        
+    @IBAction func didTapCameraButton() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
     }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+            UIImage else {
+            return
+        }
+        imageView.image = image
+    }
+}

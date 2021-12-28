85c176bc9723d682e19ca13454e7a99414cd0c76
Kam <kamran.hssn05@gmail.com>
2021-12-28 00:16:14 -0800

    Update GUI

---
+++ StatsScanner/CellViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CellViewController.swift
+//  StatsScanner
+//
+//  Created by Kamran on 12/27/21.
+//
+
+import Foundation
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    override func layoutSubviews() {
+        // cell rounded section
+        self.layer.cornerRadius = 15.0
+        self.layer.borderWidth = 5.0
+        self.layer.borderColor = UIColor.clear.cgColor
+        self.layer.masksToBounds = true
+        
+        // cell shadow section
+        self.contentView.layer.cornerRadius = 15.0
+        self.contentView.layer.borderWidth = 5.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+        self.layer.shadowColor = UIColor.white.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0.0)
+        self.layer.shadowRadius = 6.0
+        self.layer.shadowOpacity = 0.6
+        self.layer.cornerRadius = 15.0
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+    }
+}

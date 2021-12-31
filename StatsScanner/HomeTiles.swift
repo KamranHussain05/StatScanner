8acbbe712d71f702b8d036b0a6cbbdc4928157b2
Kam <kamran.hssn05@gmail.com>
2021-12-31 00:01:44 -0800

    Update Storyboard

---
+++ StatsScanner/HomeTiles.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTiles.swift
+//  StatsScanner
+//
+//  Created by Kamran on 12/30/21.
+//
+
+import UIKit
+
+class HomeTiles: UICollectionViewCell {
+    
+    @IBOutlet var myImageView: UIImageView!
+    @IBOutlet weak var dataSetName: UILabel!
+    @IBOutlet weak var creationDate: UILabel!
+    @IBOutlet weak var openDataSet: UILabel!
+    
+    
+}

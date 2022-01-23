b91b323d36e85f60ae75e57221d56530469236ce
Kamran Hussain <kamran.hssn05@gmail.com>
2022-01-22 22:25:34 -0800

    Update PlotViewController.swift

--- StatsScanner/Graph Controllers/PlotViewController.swift
+++ StatsScanner/Graph Controllers/PlotViewController.swift
@@ -49,27 +49,27 @@ class LinePlotViewController: UIViewController, AAChartViewDelegate {
             .chartType(type) //Can be any of the chart types listed under `AAChartType`.
             .animationType(.easeInSine)
             .dataLabelsEnabled(false) //Enable or disable the data labels. Defaults to false
-            .categories(["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-                         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+//            .categories(["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+//                         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
             .categories(xvalsFormatted())
             .colorsTheme(["#fe117c","#ffc069","#06caf4","#7dffc0"])
             .title(self.data.name)
             .backgroundColor("#ffffff")
-            .series([
-                AASeriesElement()
-                    .name("Tokyo")
-                    .data([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]),
-                AASeriesElement()
-                    .name("New York")
-                    .data([0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]),
-                AASeriesElement()
-                    .name("Berlin")
-                    .data([0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]),
-                AASeriesElement()
-                    .name("London")
-                    .data([3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]),
-                    ])
-            //.series(formattedData())
+//            .series([
+//                AASeriesElement()
+//                    .name("Tokyo")
+//                    .data([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]),
+//                AASeriesElement()
+//                    .name("New York")
+//                    .data([0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]),
+//                AASeriesElement()
+//                    .name("Berlin")
+//                    .data([0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]),
+//                AASeriesElement()
+//                    .name("London")
+//                    .data([3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]),
+//                    ])
+            .series(formattedData())
         //The chart view object calls the instance object of AAChartModel and draws the final graphic
         aaChartView.aa_drawChartWithChartModel(aaChartModel)
     }
@@ -86,18 +86,18 @@ class LinePlotViewController: UIViewController, AAChartViewDelegate {
     
     private func xvalsFormatted() -> [String] {
         var result = [String]()
-        let ds = data.getData()
+        let ds = data.getData(axis: 0)
         for i in 0...ds.count-1 {
-            
+            result.append(String(ds[i]))
         }
-        return []
+        return result
     }
     
     private func formattedData() -> [AASeriesElement] {
         var arr = [AASeriesElement]()
         let d = data.getData()
         
-        for i in 0...d.count-1 {
+        for i in 0...d[0].count-1 {
             arr.append(AASeriesElement()
                         .name(self.data.getKeys()[i])
                         .data(data.getData(axis:i))

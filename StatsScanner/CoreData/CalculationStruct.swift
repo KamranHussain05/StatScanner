e4940fa2d15bbca5ba4f5617005952dbcc52d848
Kamran Hussain <kamran.hssn05@gmail.com>
2022-06-07 18:38:52 -0700

    Refactor Project

---
+++ StatsScanner/CoreData/CalculationStruct.swift
@@ -0,0 +1,19 @@
+//
+//  CalculationStruct.swift
+//  StatsScanner
+//
+//  Created by Kamran Hussain on 6/7/22.
+//
+
+import Foundation
+
+struct DatasetCalculations {
+    var mean : Double = 0.0
+    var mode : Double = 0.0
+    var median : Double = 0.0
+    var range : Double = 0.0
+    var standardDeviation : Double = 0.0
+    var standardError : Double = 0.0
+    var max : Double = 0.0
+    var min : Double = 0.0
+}

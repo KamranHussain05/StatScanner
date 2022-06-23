3a2fc074b9c1e297803ba0b1e1327f77b6b791e0
Cpun077 <70039430+Cpun077@users.noreply.github.com>
2022-06-22 19:08:29 -0700

    Prototype statistic view
    test value 123 shown

--- StatsScanner/Dataset View Controllers/StatsViewController.swift
+++ StatsScanner/Dataset View Controllers/StatsViewController.swift
@@ -74,22 +74,37 @@ class DataSetViewController: UIViewController {
 }
 
 // Created by Caden 6/22/22
+//MARK: View Controller
 
 class StatsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    private var datasetobj: Dataset!
     
-    @IBOutlet var hey: UIButton!
-    
-    @IBAction func onBackClick(_sender:UIButton) {
-        print("hi")
-    }
+    private var name: String!
+    private var date: String!
+    private var items: String!
+    private var mean: String!
+    private var median: String!
+    private var mode: String!
+    private var min: String!
+    private var max: String!
+    private var range: String!
+    private var dev: String!
+    private var error: String!
     
     private let tableView : UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
-        table.register(UITableViewCell.self, forCellReuseIdentifier: StatsCell.identifier)
+        table.register(StatsCell.self, forCellReuseIdentifier: StatsCell.identifier)
         return table
     }()
     
-    var models = [format]()
+    var models = [section]()
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(setDataSetObject(_:)), name: Notification.Name("datasetobj"), object: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -99,33 +114,86 @@ class StatsViewController: UIViewController, UITableViewDataSource, UITableViewD
         tableView.dataSource = self
         tableView.frame = view.bounds
         view.addSubview(tableView)
+        NotificationCenter.default.addObserver(self, selector: #selector(setDataSetObject(_:)), name: Notification.Name("datasetobj"), object: nil)
+        loadData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        loadData()
+    }
+    
+    @objc func setDataSetObject(_ notification: Notification) {
+        print("statistics view recieved dataset")
+        datasetobj = (notification.object as! Dataset)
+    }
+    
+    func loadData() {
+        name = datasetobj.name
+        date = datasetobj.creationDate
+        items = String(datasetobj.getTotalNumItems())
+        mean = String(round(1000 * datasetobj.calculations[7]) / 1000)
+        median = String(datasetobj.calculations[3])
+        mode = String(datasetobj.calculations[2])
+        min = String(datasetobj.calculations[1])
+        max = String(datasetobj.calculations[0])
+        range = String(datasetobj.calculations[4])
+        dev = String(round(1000 * datasetobj.calculations[5]) / 1000)
+        error = String(round(1000 * datasetobj.calculations[6]) / 1000)
     }
     
     func configure() {
+        models.append(section(title: "Info", options: [format(title: "Name") {}, format(title: "Creation Date") {}, format(title: "Data Points") {}]))
+        models.append(section(title: "Averages", options: [format(title: "Mean") {}, format(title: "Median") {}, format(title: "Mode") {}]))
+        models.append(section(title: "Scope", options: [format(title: "Min") {}, format(title: "Max") {}, format(title: "Range") {}]))
+        models.append(section(title: "Errors", options: [format(title: "Standard Deviation") {}, format(title: "Standard Error") {}]))
+    }
+    
+    func calculate() {
         
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return models.count
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].options.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let model = models[section]
+        return model.title
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = models[indexPath.row]
+        let model = models[indexPath.section].options[indexPath.row]
         guard let cell = tableView.dequeueReusableCell(withIdentifier: StatsCell.identifier, for: indexPath) as? StatsCell else {
             return UITableViewCell()
         }
         cell.configure(with: model)
-        cell.textLabel?.text = model.title
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = models[indexPath.section].options[indexPath.row]
+        model.handler()
     }
 }
 
+//MARK: Structs
+
 struct format {
     let title: String
-    let icon: UIImage?
-    let iconBackgroundColor: UIColor
     var handler: (()-> Void)
 }
+
+struct section {
+    let title: String
+    let options: [format]
+}
+
+//MARK: Stats Cells
 
 class StatsCell: UITableViewCell {
     static let identifier = "StatsCell"
@@ -136,10 +204,18 @@ class StatsCell: UITableViewCell {
         return label
     }()
     
+    private let numbers : UILabel = {
+        let numbers = UILabel()
+        numbers.text = "123"
+        return numbers
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(label)
         contentView.clipsToBounds = true
+        numbers.sizeToFit()
+        accessoryView = numbers
     }
     
     required init?(coder: NSCoder) {
@@ -148,8 +224,7 @@ class StatsCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        let size: CGFloat = contentView.frame.size.height - 12
-        label.frame = CGRect(x: 5, y: 0, width: contentView.frame.size.width - 5, height: contentView.frame.size.height)
+        label.frame = CGRect(x: 20, y: 0, width: contentView.frame.size.width - 5, height: contentView.frame.size.height)
     }
     
     override func prepareForReuse() {
@@ -159,6 +234,5 @@ class StatsCell: UITableViewCell {
     
     public func configure(with model: format) {
         label.text = model.title
-        
     }
 }

3f2a1174b6e3c0715491fdb9b7eea51cccf217af
Cpun077 <70039430+Cpun077@users.noreply.github.com>
2022-06-30 13:55:59 -0700

    Fixed landscape hometile spacing

--- StatsScanner/Home View Controllers/HomeViewController.swift
+++ StatsScanner/Home View Controllers/HomeViewController.swift
@@ -33,12 +33,13 @@ class HomeViewController: UIViewController, UIDocumentPickerDelegate {
 		let layout = UICollectionViewFlowLayout()
 		//let width = (view.frame.size.width*(1-3*sc))/2
 		//let height = (view.frame.size.width*(1.25-sc))/2
+		// 414 is width of iphone11 screen
 		let width = (414*(1-3*sc))/2
 		let height = (414*(1.25-sc))/2
 		layout.itemSize = CGSize(width: width, height: height)
-		layout.minimumLineSpacing = sc*view.frame.size.width
-		layout.minimumInteritemSpacing = sc*view.frame.size.width
-		layout.sectionInset = UIEdgeInsets(top: 0.02*view.frame.size.width, left: sc*view.frame.size.width, bottom: 0, right: sc*view.frame.size.width)
+		layout.minimumLineSpacing = sc*414
+		layout.minimumInteritemSpacing = sc*414
+		layout.sectionInset = UIEdgeInsets(top: 0.02*view.frame.size.width, left: sc*414, bottom: 0, right: sc*414)
 		myCollectionView.collectionViewLayout = layout
         getAllItems()
         

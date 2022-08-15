1368e064154c11ddb4e9e4e7e185f6dc4bb07a4f
Cpun077 <cadenpun@gmail.com>
2022-08-15 12:04:43 -0700

    Version There are multiple projects in this directory. agvtool does not work with multiple projects in the same directory. agvtool - Apple-generic versioning tool for Xcode projects usage: agvtool help agvtool what-version | vers [-terse] agvtool [-noscm | -usecvs | -usesvn] next-version | bump [-all] agvtool [-noscm | -usecvs | -usesvn] new-version [-all] <versNum> agvtool [-noscm | -usecvs | -usesvn] tag [-force | -F] [-noupdatecheck | -Q] [-baseurlfortag] agvtool what-marketing-version | mvers [-terse | -terse1] agvtool [-noscm | -usecvs | -usesvn] new-marketing-version <versString> (There are multiple projects in this directory. agvtool does not work with multiple projects in the same directory. agvtool - Apple-generic versioning tool for Xcode projects usage: agvtool help agvtool what-version | vers [-terse] agvtool [-noscm | -usecvs | -usesvn] next-version | bump [-all] agvtool [-noscm | -usecvs | -usesvn] new-version [-all] <versNum> agvtool [-noscm | -usecvs | -usesvn] tag [-force | -F] [-noupdatecheck | -Q] [-baseurlfortag] agvtool what-marketing-version | mvers [-terse | -terse1] agvtool [-noscm | -usecvs | -usesvn] new-marketing-version <versString>)

--- StatsScanner/Home View Controllers/HomeViewController.swift
+++ StatsScanner/Home View Controllers/HomeViewController.swift
@@ -37,7 +37,7 @@ class HomeViewController: UIViewController, UIDocumentPickerDelegate, UIImagePic
 		layout.itemSize = CGSize(width: (414*(1-3*sc))/2, height: (414*(1.25-sc))/2)
 		layout.minimumLineSpacing = sc*414
 		layout.minimumInteritemSpacing = sc*414
-		layout.sectionInset = UIEdgeInsets(top: 0.02*view.frame.size.width, left: sc*400, bottom: 0, right: sc*400)
+		layout.sectionInset = UIEdgeInsets(top: 0.02*view.frame.size.width, left: 0, bottom: 0, right: 0)
 		myCollectionView.collectionViewLayout = layout
         getAllItems()
         

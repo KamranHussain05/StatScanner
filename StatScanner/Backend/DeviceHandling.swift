a0ea3484b5696bc2abf953b65ee7fed1991002d3
Kamran Hussain <kamran.hssn05@gmail.com>
2023-01-29 14:29:28 -0800

    Add Documentation

--- StatScanner/Backend/DeviceHandling.swift
+++ StatScanner/Backend/DeviceHandling.swift
@@ -4,27 +4,41 @@
 //
 //  Created by Kamran Hussain on 8/16/22.
 //
+// Functions and extensions for device specific operations.
 
 import Foundation
 import UIKit
 
+/// Check if the app is running on a device running iOS by checking the file directory structure.
+///
+/// - Returns: Type ``Bool`` that presents True if an iOS device is detected and False otherwise.
+/// - Note: This function may work improperly in the future if file structures are changed
+/// - Authors: Kamran Hussain
+///
 func isPhone() -> Bool {
     let fone = URL(string: DataBridge.getDocumentsDirectory().absoluteString.replacingOccurrences(of: "file://", with: "shareddocuments://"))!
     return UIApplication.shared.canOpenURL(fone)
 }
 
+/// File reading or writing error that is thrown when an error occurs in the DataBridge reading or writing process. Catch this error and display a message to the user accordingly.
+/// - Authors: Kamran Hussain
 enum FileIOError : Error {
     case UnrecognizedFile
     case CorruptedFile
     case ReadingError
 }
 
+/// Collection iterator extension that interates through an array and cleans out empty (nil) values.
+///
+/// - Parameters: Extend this in a class then use the standard interation subscript.
+/// - Authors: Kamran Hussain
 extension Collection where Indices.Iterator.Element == Index {
     subscript (safe index: Index) -> Iterator.Element? {
         return indices.contains(index) ? self[index] : nil
     }
 }
 
+/// Extensions for view controller gesture recognitions
 extension UIViewController {
     
     func hideKeyboardOnTap() {
@@ -39,6 +53,9 @@ extension UIViewController {
     }
 }
 
+/// Extensions for the entire application to iterate build and version numbers and display them in app
+///
+/// - Authors: Caden Pun
 extension UIApplication {
     class func vers() -> String {
         return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
